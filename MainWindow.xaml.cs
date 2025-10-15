d2f6320a220952f782c782e903114928bf150e4f
Vasek-Doskar <doskar@spsjc.cz>
2025-10-15 11:25:42 +0200

    ed2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 using ToDoApp_I3B2.Database;
 using ToDoApp_I3B2.Manager;
+using ToDoApp_I3B2.Model;
 
 namespace ToDoApp_I3B2
 {
@@ -10,17 +12,20 @@ namespace ToDoApp_I3B2
     public partial class MainWindow : Window
     {
         private readonly ToDoManager _manager;
+        public ObservableCollection<ToDo> Todos { get; set; } // automaticky se refreshující kolekce
         public MainWindow()
         {
             _manager = new ToDoManager(new ToDoDbContext());
-
+            Todos = new ObservableCollection<ToDo>(_manager.GetAll());
             InitializeComponent(); // od tohoto okamžiku je propojeno GUI (PŘIPOJILO TLAČÍTKO)
-            //ToDoListBox.ItemsSource;
+            ToDoListBox.ItemsSource = Todos;
 
         }
 
-       
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
 
+        }
     }
 
 }
